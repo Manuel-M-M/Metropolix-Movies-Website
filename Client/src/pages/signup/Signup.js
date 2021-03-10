8c567a0fd7b0ab30036a749de7c67e7ce95c4d0a
Manuel <mmoragamolina@gmail.com>
2021-03-10 11:39:25 +0100

    designing user page

--- Client/src/pages/signup/Signup.js
+++ Client/src/pages/signup/Signup.js
@@ -4,59 +4,59 @@ import '../signin/Signup-Signin.css';
 import AlertContext from "../../context/alerts/AlertContext";
 import AuthContext from "../../context/auth/AuthContext";
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
-function Signup({setUer}){
-    const [username, setusername] = useState("");
-    const [email, setemail] = useState("");
-    const [password, setpassword] = useState("");
+ function Signup({setUer}){
+//     const [username, setusername] = useState("");
+//     const [email, setemail] = useState("");
+//     const [password, setpassword] = useState("");
 
-    const handleSubmit = (event)=>{
-        event.preventDefault();
+//     const handleSubmit = (event)=>{
+//         event.preventDefault();
 
-        const formData = new FormData();
-        formData.append('username', username);
-        formData.append('email', email);
-        formData.append('password', password);
+//         const formData = new FormData();
+//         formData.append('username', username);
+//         formData.append('email', email);
+//         formData.append('password', password);
 
-        fetch('http://localhost:8000/contacto', {
-            method: 'POST',
-            mode: 'cors',
-            body: formData
-        })
-        .then(response => response.json())
-        .then(
-            resp => {
-                console.log(resp);
-                setContactos(users => {
-                    const newArray = users.slice();
-                    newArray.push(resp);
-                    return newArray;
-                });
+//         fetch('http://localhost:8000/contacto', {
+//             method: 'POST',
+//             mode: 'cors',
+//             body: formData
+//         })
+//         .then(response => response.json())
+//         .then(
+//             resp => {
+//                 console.log(resp);
+//                 setContactos(users => {
+//                     const newArray = users.slice();
+//                     newArray.push(resp);
+//                     return newArray;
+//                 });
 
-                // vaciar los campos:
-                setUsername("");
-                setEmail("");
-                setPassword("");
+//                 // vaciar los campos:
+//                 setUsername("");
+//                 setEmail("");
+//                 setPassword("");
                 
-            } 
-        ).catch( error => console.log(error) );
+//             } 
+//         ).catch( error => console.log(error) );
         
-    }
+//     }
 
-    const handleUsername = (evento)=>{
-        setUsername(evento.target.value);
-    }
-    const handleEmail = (evento)=>{
-        setEmail(evento.target.value);
-    }
-    const handlePassword = (evento)=>{
-        setPassword(evento.target.value);
-    }
-    const handlePassword = (evento)=>{
-        setPassword(evento.target.value);
-    }
-}
+//     const handleUsername = (evento)=>{
+//         setUsername(evento.target.value);
+//     }
+//     const handleEmail = (evento)=>{
+//         setEmail(evento.target.value);
+//     }
+//     const handlePassword = (evento)=>{
+//         setPassword(evento.target.value);
+   // }
+    // const handlePassword = (evento)=>{
+    //     setPassword(evento.target.value);
+    // }
+//}
     
 
 
@@ -128,60 +128,61 @@ function Signup({setUer}){
 
     
     return (
-        <div className="form-usuario">
-            { alert ? (<div className={`alert ${alert.category}`}>{alert.message}</div>) : null }
-            <div className="contenedor-form sombra-dark">
-                <h1>Sign up</h1>
-                <form onSubmit={onSubmit}>
-                    <div className="campo-form">
-                        <label htmlFor="username">Username</label>
-                        <input 
-                            type="text" 
-                            placeholder="Your username"
-                            value={username}
-                            onChange={handleUsername}
-                        />
-                    </div>
-                    <div className="campo-form">
-                        <label htmlFor="email">Email</label>
-                        <input 
-                            type="email"
-                            placeholder="Your email"
-                            value={email}
-                            onChange={handleEmail}
-                        />
-                    </div>
-                    <div className="campo-form">
-                        <label htmlFor="password">Password</label>
-                        <input 
-                            type="password"
-                            placeholder="Your password"
-                            value={password}
-                            onChange={handlePassword}
-                        />
-                    </div>
-                    <div className="campo-form">
-                        <label htmlFor="confirm">Confirm Password</label>
-                        <input 
-                            type="password"
-                            id="confirm"
-                            name="confirm"
-                            placeholder="Repeat your password"
-                            value={confirm}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div className="campo-form">
-                        <input type="submit" className="btn-sign btn-primario btn-block"
-                        value="Sign Up" />
-                    </div>
-                </form>
+        // <div className="form-usuario">
+        //     { alert ? (<div className={`alert ${alert.category}`}>{alert.message}</div>) : null }
+        //     <div className="contenedor-form sombra-dark">
+        //         <h1>Sign up</h1>
+        //         <form onSubmit={onSubmit}>
+        //             <div className="campo-form">
+        //                 <label htmlFor="username">Username</label>
+        //                 <input 
+        //                     type="text" 
+        //                     placeholder="Your username"
+        //                     value={username}
+        //                     onChange={handleUsername}
+        //                 />
+        //             </div>
+        //             <div className="campo-form">
+        //                 <label htmlFor="email">Email</label>
+        //                 <input 
+        //                     type="email"
+        //                     placeholder="Your email"
+        //                     value={email}
+        //                     onChange={handleEmail}
+        //                 />
+        //             </div>
+        //             <div className="campo-form">
+        //                 <label htmlFor="password">Password</label>
+        //                 <input 
+        //                     type="password"
+        //                     placeholder="Your password"
+        //                     value={password}
+        //                     onChange={handlePassword}
+        //                 />
+        //             </div>
+        //             <div className="campo-form">
+        //                 <label htmlFor="confirm">Confirm Password</label>
+        //                 <input 
+        //                     type="password"
+        //                     id="confirm"
+        //                     name="confirm"
+        //                     placeholder="Repeat your password"
+        //                     value={confirm}
+        //                     onChange={onChange}
+        //                 />
+        //             </div>
+        //             <div className="campo-form">
+        //                 <input type="submit" className="btn-sign btn-primario btn-block"
+        //                 value="Sign Up" />
+        //             </div>
+        //         </form>
                 
-                <Link to={'/Signin'} className="account-link">
-                   Go to Sign in
-                </Link>
-            </div>
-        </div>
+        //         <Link to={'/Signin'} className="account-link">
+        //            Go to Sign in
+        //         </Link>
+        //     </div>
+        //</div>
+        <h1>signup</h1>
     )
 }
 
--- Client/src/pages/user/User.js
+++ Client/src/pages/user/User.js
@@ -1,10 +1,50 @@
 import UserHeader from '../../components/userHeader/UserHeader';
 import './User.css';
+import HeaderBody from '../../components/headerBody/HeaderBody';
 
 function User () {
 
     return (
-        <UserHeader />
+        <>
+            <div className="container">
+                <HeaderBody title="User Space"/>
+                {/* <div className="wrap-btn"> */}
+                    <div className="btn-delete">
+                        <button className="btn btn-transparent mr-2"
+                        // onClick={() => {
+                        //     window.history.back();
+                        // }}
+                        >Delete your account
+                        </button>
+                    </div>
+                {/* </div> */}
+                <div className="img-head">
+                    <img src="../../img/backgrounds/Collage.jpg" alt=""/>
+                </div>
+                <div className="user-text">
+                    <h6>
+                    Welcome to the temple of cinema. Now you can manage your movies and comment on them.
+                    If you really are a lover of the seventh art, enter our <a href="#">REAL TIME CHAT</a> and
+                    participate in our cinephile community.
+                    </h6>
+                    <h5>
+                        “A film is never really good unless the camera is an eye in the head of a poet.”
+                        - Orson Welles
+                    </h5>
+                </div>
+                <div className="user-header">
+                    <UserHeader />
+                </div>
+               
+                
+                {/* <div className="row justify-content-center mt-3 mb-3">
+                    <button className="btn btn-transparent mr-1" >-</button>
+                    <button className="btn btn-transparent ml-1" >+</button>
+                </div> */}
+                
+            </div>
+        </>
+        
     );
 }
 
